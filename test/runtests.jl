5bf060965297f366065c108461965d06483f40ea
xtalax <alex.lemony@gmail.com>
2023-03-23 18:23:07 +0000

    Downstream Tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,13 @@
-# Currently verified by MOL, Downstream tests to be added
+using Pkg
+using SafeTestsets
+
+const GROUP = get(ENV, "GROUP", "All")
+
+const is_APPVEYOR = Sys.iswindows() && haskey(ENV, "APPVEYOR")
+
+const is_TRAVIS = haskey(ENV, "TRAVIS")
+
+const is_CI = haskey(ENV, "CI")
+
+# Currently verified by Downstream tests
 @test true
